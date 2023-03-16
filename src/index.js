675cb4f349b03c131eb60c07914331501c4995bf
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-16 12:12:23 +0500

    add create customer  api and make one on one relation between customer and identifier

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
-app.listen(process.env.PORT, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
+var bodyParser = require("body-parser");
+const { Customer, Identifier } = require("./models/userModel");
+app.use(bodyParser.json());
+app.post("/create-customer", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newCustomer = await Customer.create(req.body);
+    const newIdentifier = await Identifier.create({
+      customerCode: newCustomer._id.toString(),
+      customer: newCustomer,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        cutomer: newCustomer,
+        Identifier: newIdentifier,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 mongoose
   .connect(process.env.DATABASE_URI)
@@ -14,3 +32,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
